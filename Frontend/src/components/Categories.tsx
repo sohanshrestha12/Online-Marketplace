bc3ce8063f396b859b3a08b0fc8bea62429eb27d
sohanshrestha12 <sohanshrestha40@gmail.com>
2024-06-26 22:46:51 +0545

    fix category home page

--- Frontend/src/components/Categories.tsx
+++ Frontend/src/components/Categories.tsx
@@ -1,38 +1,46 @@
+import { Category } from "@/Types/Category";
 import React from "react";
 
-interface Category {
-  name: string;
-  icon: JSX.Element;
-  subcategories: string[];
-}
 
 interface CategoriesProps {
   categories: Category[];
 }
+
+const shuffleArray = (array: Category[]) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
 const Categories: React.FC<CategoriesProps> = ({ categories }) => {
+  const shuffledCategories = shuffleArray([...categories]);
   return (
     <div className="mt-5 max-w-screen-2xl">
       <h3 className="text-lg font-semibold mb-4">Categories</h3>
       <div className="grid grid-cols-12">
-        {categories.map((item, i) => (
-          <div
-            className="col-span-2 gap-1 h-[150px] w-[180px] hover:cursor-pointer hover:shadow-md bg-white hover:border-b"
-            key={i}
-          >
-            <div className="flex justify-center flex-col gap-3 items-center w-full ">
-              <div className="w-[80px] h-[80px] ">
-                <img
-                  className="h-full w-full object-cover"
-                  src={
-                    "https://images.unsplash.com/photo-1718804714822-8b81342a5e9d?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                  }
-                  alt="404 categories"
-                />
+        {shuffledCategories
+          .filter((cate) => cate.level === 2)
+          .splice(0, 12)
+          .map((item, i) => (
+            <div
+              className="col-span-2 gap-1 h-[150px] w-[180px] hover:cursor-pointer hover:shadow-md bg-white hover:border-b"
+              key={i}
+            >
+              <div className="flex justify-center flex-col gap-3 items-center w-full ">
+                <div className="w-[80px] h-[80px] ">
+                  <img
+                    className="h-full w-full object-cover"
+                    src={
+                      "https://images.unsplash.com/photo-1718804714822-8b81342a5e9d?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    }
+                    alt="404 categories"
+                  />
+                </div>
+                {item.name}
               </div>
-              {item.subcategories[0]}
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   );
--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,150 +1,197 @@
+import { Category } from "@/Types/Category";
+import { getAllCategories } from "@/api/Product";
 import CarouselComponent from "@/components/Carousel";
 import Categories from "@/components/Categories";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaChevronRight } from "react-icons/fa";
 import {
-  FiUser,
-  FiHeart,
-  FiBox,
-  FiClock,
-  FiSmartphone,
-  FiTv,
-  FiHeadphones,
-  FiShoppingCart,
-  FiGift,
-  FiHome,
   FiActivity,
+  FiClock,
+  FiGift,
+  FiHeadphones,
+  FiHeart,
+  FiHome,
+  FiShoppingCart,
+  FiSmartphone,
   FiTool,
+  FiTv
 } from "react-icons/fi";
+import { GiPirateCoat } from "react-icons/gi";
+import { PiDressLight } from "react-icons/pi";
 
 const Home = () => {
-  const [activeCategory, setActiveCategory] = useState<number | null>(null);
+  const [allCategory, setAllCategory] = useState<Category[]>([]);
+  const [level1Category, setLevel1Category] = useState<Category[]>([]);
+  const [level2Category, setLevel2Category] = useState<Category[]>([]);
+  const [level3Category, setLevel3Category] = useState<Category[]>([]);
+  const [activeCategory, setActiveCategory] = useState<string | null>(null);
+  const [activeSubCategory, setActiveSubCategory] = useState<string | null>(
+    null
+  );
   const [hoverTimeout, setHoverTimeout] = useState<NodeJS.Timeout | null>(null);
+  const [subHoverTimeout, setSubHoverTimeout] = useState<NodeJS.Timeout | null>(null);
 
-  const handleCategoryHover = (index: number) => {
+  useEffect(() => {
+    const fetchCategory = async () => {
+      try {
+        const response = await getAllCategories();
+        setAllCategory(response.data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchCategory();
+  }, []);
+
+  useEffect(() => {
+    setLevel1Category(allCategory.filter((item) => item.level === 1));
+    setLevel2Category(allCategory.filter((item) => item.level === 2));
+    setLevel3Category(allCategory.filter((item) => item.level === 3));
+  }, [allCategory]);
+
+  const handleSubCategoryHover = (name: string) => {
+     if (subHoverTimeout) {
+       clearTimeout(subHoverTimeout);
+     }
+     if (hoverTimeout) {
+       clearTimeout(hoverTimeout);
+     }
+    setActiveSubCategory(name);
+  };
+
+  const handleSubCategoryLeave =() =>{
+     const subTimeout = setTimeout(() => {
+       setActiveSubCategory(null);
+     }, 200);
+     setSubHoverTimeout(subTimeout);
+  }
+
+  const handleCategoryHover = (name: string) => {
     if (hoverTimeout) {
       clearTimeout(hoverTimeout);
     }
-    setActiveCategory(index);
+    setActiveCategory(name);
+    setActiveSubCategory(null);
   };
 
   const handleCategoryLeave = () => {
     const timeout = setTimeout(() => {
+      setActiveSubCategory(null);
       setActiveCategory(null);
     }, 200);
     setHoverTimeout(timeout);
   };
 
-  const categories = [
-    {
-      name: "Women's Fashion",
-      icon: <FiUser />,
-      subcategories: ["Tops", "Bottoms", "Dresses", "Shoes", "Accessories"],
-    },
-    {
-      name: "Health & Beauty",
-      icon: <FiHeart />,
-      subcategories: ["Skincare", "Makeup", "Haircare", "Personal Care"],
-    },
-    {
-      name: "Men's Fashion",
-      icon: <FiBox />,
-      subcategories: ["Shirts", "Pants", "Shoes", "Accessories"],
-    },
-    {
-      name: "Watches & Accessories",
-      icon: <FiClock />,
-      subcategories: ["Watches", "Jewelry", "Eyewear"],
-    },
-    {
-      name: "Electronic Devices",
-      icon: <FiSmartphone />,
-      subcategories: ["Smartphones", "Laptops", "Tablets", "Wearables"],
-    },
-    {
-      name: "TV & Home Appliances",
-      icon: <FiTv />,
-      subcategories: [
-        "Televisions",
-        "Refrigerators",
-        "Washing Machines",
-        "Microwaves",
-      ],
-    },
-    {
-      name: "Electronic Accessories",
-      icon: <FiHeadphones />,
-      subcategories: ["Headphones", "Chargers", "Cables"],
-    },
-    {
-      name: "Groceries & Pets",
-      icon: <FiShoppingCart />,
-      subcategories: ["Food", "Pet Supplies", "Home Essentials"],
-    },
-    {
-      name: "Barbies & Toys",
-      icon: <FiGift />,
-      subcategories: ["Dolls", "Action Figures", "Board Games", "Outdoor Toys"],
-    },
-    {
-      name: "Home & Lifestyle",
-      icon: <FiHome />,
-      subcategories: ["Furniture", "Decor", "Kitchenware", "Bedding"],
-    },
-    {
-      name: "Sports & Outdoor",
-      icon: <FiActivity />,
-      subcategories: ["Sportswear", "Fitness Equipment", "Outdoor Gear"],
-    },
-    {
-      name: "Motors, Tools & DIY",
-      icon: <FiTool />,
-      subcategories: ["Automotive", "Tools", "Home Improvement"],
-    },
-  ];
+
+  const iconMapping: { [key: string]: JSX.Element } = {
+    "Women's Fashion": <PiDressLight />,
+    "Health & Beauty": <FiHeart />,
+    "Men's Fashion": <GiPirateCoat />,
+    "Watches & Accessories": <FiClock />,
+    "Electronic Devices": <FiSmartphone />,
+    "TV & Home Appliances": <FiTv />,
+    "Electronic Accessories": <FiHeadphones />,
+    "Groceries & Pets": <FiShoppingCart />,
+    "Barbies & Toys": <FiGift />,
+    "Home & Lifestyle": <FiHome />,
+    "Sports & Outdoor": <FiActivity />,
+    "Motors, Tools & DIY": <FiTool />,
+  };
 
   return (
     <>
       <div className="grid grid-cols-12 gap-3 max-w-screen-2xl">
-        <div className="col-span-3 bg-[#fffefe] shadow-md py-3 flex flex-col gap-1 rounded-lg relative">
-          {categories.map((item, i) => (
+        <div className="col-span-3 bg-[#fffefe] shadow-md py-3 justify-center flex flex-col gap-1 rounded-lg relative">
+          {level1Category.map((item, i) => (
             <div
               key={i}
               className="flex gap-3 items-center px-3 hover:bg-[#e4e3e3] py-1 transition-all justify-between hover:cursor-pointer font-medium"
-              onMouseEnter={() => handleCategoryHover(i)}
+              onMouseEnter={() => handleCategoryHover(item.name)}
               onMouseLeave={handleCategoryLeave}
             >
               <div className="flex gap-3 items-center">
                 <span
-                  className={`${activeCategory === i ? "text-[#f85606]" : ""}`}
+                  className={`${
+                    activeCategory === item.name ? "text-[#f85606]" : ""
+                  }`}
                 >
-                  {item.icon}
+                  {iconMapping[item.name]}
                 </span>
                 <p
                   className={`text-sm m-0 ${
-                    activeCategory === i ? "text-[#f85606]" : ""
+                    activeCategory === item.name ? "text-[#f85606]" : ""
                   }`}
                 >
                   {item.name}
                 </p>
               </div>
-              {activeCategory === i && (
+              {activeCategory === item.name && (
                 <FaChevronRight
                   className={`text-sm ${
-                    activeCategory === i ? "text-[#f85606]" : ""
+                    activeCategory === item.name ? "text-[#f85606]" : ""
                   }`}
                 />
               )}
-              {activeCategory === i && (
-                <ul className="absolute left-[102%] z-10 top-0 h-full flex flex-col gap-1 py-3 w-[200px] shadow-md bg-[#fffefe] rounded-md p-2">
-                  {item.subcategories.map((subcategory, index) => (
-                    <li
-                      key={index}
-                      className="text-sm hover:text-[#f85606] px-3 transition-all py-1 hover:bg-[#e4e3e3]"
-                    >
-                      {subcategory}
-                    </li>
-                  ))}
+              {activeCategory === item.name && (
+                <ul
+                  onMouseEnter={() => {
+                    if (subHoverTimeout) clearTimeout(subHoverTimeout);
+                  }}
+                  onMouseLeave={handleSubCategoryLeave}
+                  className="absolute left-[102%] z-10 top-0 h-full flex flex-col gap-1 py-3 w-[200px] shadow-md bg-[#fffefe] rounded-md p-2"
+                >
+                  {level2Category
+                    .filter(
+                      (subCat) =>
+                        subCat.parent === item.name && subCat.level === 2
+                    )
+                    .map((subcategory, index) => (
+                      <div
+                        key={index}
+                        onMouseEnter={() => {
+                          handleSubCategoryHover(subcategory.name);
+                        }}
+                        className="transition-all"
+                      >
+                        <li className="text-sm hover:text-[#f85606] px-3 transition-all py-1 hover:bg-[#e4e3e3]">
+                          <p
+                            className={`${
+                              activeSubCategory === subcategory.name
+                                ? "text-[#f85606]"
+                                : ""
+                            }`}
+                          >
+                            {subcategory.name}
+                          </p>
+                        </li>
+                        {activeSubCategory === subcategory.name && (
+                          <ul className="absolute left-[102%] z-10 top-0 h-full flex flex-col transition-all gap-1 py-3 w-[200px] shadow-md bg-[#fffefe] rounded-md p-2">
+                            {level3Category.filter(
+                              (l3category) =>
+                                activeSubCategory === l3category.parent
+                            ).length > 0 ? (
+                              level3Category
+                                .filter(
+                                  (l3category) =>
+                                    activeSubCategory === l3category.parent
+                                )
+                                .map((l3cat, i) => (
+                                  <li
+                                    key={i}
+                                    className="text-sm hover:text-[#f85606] px-3 transition-all py-1 hover:bg-[#e4e3e3]"
+                                  >
+                                    {l3cat.name}
+                                  </li>
+                                ))
+                            ) : (
+                              <li className="text-sm px-3 text-center py-1 text-gray-500">
+                                No subcategories found
+                              </li>
+                            )}
+                          </ul>
+                        )}
+                      </div>
+                    ))}
                 </ul>
               )}
             </div>
@@ -156,7 +203,7 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <Categories categories={categories} />
+      <Categories categories={allCategory} />
     </>
   );
 };
